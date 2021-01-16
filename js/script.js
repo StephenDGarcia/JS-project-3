67354fa1e36ffbf11856a375ab71ea81a63580d3
StephenDGarcia <stephendgarcia@gmail.com>
2021-01-16 10:01:41 -0600

    Update script.js
    getting started. stops at line 4 with an error

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('test');
+//let name = document.getElementById('name')
+let name = document.querySelector('[name="user-name"]');
+console.log(name);
+name.focus() = true;
+
+let jobRole = document.getElementById('title');
+console.log(jobRole)
+
+let otherJobRole = document.getElementById('other-job-role');
+console.log(otherJobRole)
