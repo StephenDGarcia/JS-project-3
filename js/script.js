5136fa64f9909b15cc04fd9de641e6de941154df
StephenDGarcia <stephendgarcia@gmail.com>
2021-01-19 23:56:03 -0600

    other-job-role
    visibility on other-job-role

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-//let name = document.getElementById('name')
-let name = document.querySelector('[name="user-name"]');
+
+let name = document.getElementById('name')
+//let name = document.querySelector('[name="user-name"]');
 console.log(name);
-name.focus() = true;
+name.focus()
 
 let jobRole = document.getElementById('title');
 console.log(jobRole)
 
 let otherJobRole = document.getElementById('other-job-role');
-console.log(otherJobRole)
+console.log(otherJobRole);
+otherJobRole.style.visibility = 'hidden';
+
+let selectOther = document.querySelector('[value=other]');
+console.log(selectOther);
+
+// $('otherJobRole').on('click keypress', function() {
+
+selectOther.addEventListener('change', (e) => {
+   otherJobRole.style.visibility = 'visible';
+});
+// })
